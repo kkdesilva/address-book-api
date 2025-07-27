d2ba3d43c835af07e414a090bd83361c57abd481
kushan <kushan.desilva@ukcaravans4hire.com>
2025-07-27 16:46:58 +0100

    add a filter method for the public interface of ContactRepository contract.
    Also, introduce ContactData DTO and update the interface for stricter typing.

--- app/Contracts/ContactRepository.php
+++ app/Contracts/ContactRepository.php
@@ -3,12 +3,14 @@ declare(strict_types=1);
 
 namespace App\Contracts;
 
+use App\DTOs\ContactData;
 use App\Models\Contact;
 
 interface ContactRepository
 {
     public function find(string $id): ?Contact;
-    public function create(array $data): Contact;
-    public function update(string $id, array $data): ?Contact;
+    public function create(ContactData $data): Contact;
+    public function update(string $id, ContactData $data): ?Contact;
     public function delete(string $id): bool;
+    public function filter(array $filters): array;
 }
---
+++ app/DTOs/ContactData.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\DTOs;
+
+final readonly class ContactData
+{
+    public function __construct(
+        public string $first_name,
+        public string $last_name,
+        public string $email,
+        public string $phone,
+    )
+    {
+    }
+
+    public static function fromArray(array $data): self
+    {
+        return new self(
+            $data['first_name'],
+            $data['last_name'],
+            $data['email'],
+            $data['phone']
+        );
+    }
+}
