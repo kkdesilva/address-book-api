0375d28329ac92a107cec887981dcf377eebeb94
kushan <kushan.desilva@ukcaravans4hire.com>
2025-07-28 00:15:43 +0100

    test: improve GET /contacts endpoint by adding filter and empty result scenarios, and renaming test file for clarity.

---
+++ tests/Feature/ListContactApiTest.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+use Illuminate\Testing\Fluent\AssertableJson;
+
+beforeEach(function () {
+    setupTestAddressBook($this);
+});
+
+afterEach(function () {
+    cleanupTestAddressBook();
+});
+
+it('can fetch all contacts', function () {
+    $response = $this->getJson('/api/v1/contacts');
+    $response->assertOk()
+        ->assertJson(
+            fn (AssertableJson $json) => $json->has('data', 10)
+                ->has('data.0', fn (AssertableJson $json) => $json
+                    ->where('first_name', 'Alex')
+                    ->where('last_name', 'Johnson')
+                    ->where('email', 'alex.johnson@example.com')
+                    ->etc()
+                )
+        );
+});
+
+it('can filter contacts by query parma', function () {
+    $response = $this->getJson('/api/v1/contacts?first_name=Alex');
+    $response->assertOk()
+        ->assertJson(
+            fn (AssertableJson $json) => $json->has('data', 1)
+                ->has('data.0', fn (AssertableJson $json) => $json
+                    ->where('first_name', 'Alex')
+                    ->where('last_name', 'Johnson')
+                    ->where('email', 'alex.johnson@example.com')
+                    ->where('phone', '01700111222')
+                    ->etc()
+                )
+        );
+});
+
+it('can filter contacts by multiple query parma', function () {
+    $response = $this->getJson('/api/v1/contacts?first_name=Alex&phone=01700111222');
+    $response->assertOk()
+        ->assertJson(
+            fn (AssertableJson $json) => $json->has('data', 1)
+                ->has('data.0', fn (AssertableJson $json) => $json
+                    ->where('first_name', 'Alex')
+                    ->where('last_name', 'Johnson')
+                    ->where('email', 'alex.johnson@example.com')
+                    ->where('phone', '01700111222')
+                    ->etc()
+                )
+        );
+});
+
+it('returns empty array when no matching records found', function () {
+    $response = $this->getJson('/api/v1/contacts?first_name=new-contact&phone=01700111222');
+    $response->assertOk()
+        ->assertJson(
+            fn (AssertableJson $json) => $json->where('data', [])
+            ->missing('data.0')
+        );
+});
